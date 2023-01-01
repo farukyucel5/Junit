a0f679c9a20589e6ff1b7b67499e0b55d0f1cb08
Faruk Yucel <farukyucel5@gmail.com>
2023-01-02 00:14:19 +0300

    Arrow practice

--- src/test/java/AmazonPractice/Practice1FeedArrow.java
+++ src/test/java/AmazonPractice/Practice1FeedArrow.java
@@ -1,5 +1,6 @@
 package AmazonPractice;
 
+import Utilities.ReuseableMethods;
 import Utilities.TestBase;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -15,7 +16,18 @@ public class Practice1FeedArrow extends TestBase {
     public void feedArrow(){
         driver.get("https://www.amazon.com/");
 
+        Actions actions=new Actions(driver);
+        WebElement element= driver.findElement(By.xpath("(//div/ul)[1]/li[10]"));
+        actions.moveToElement(element).perform();
+        ReuseableMethods.waiting(3);
+
+        driver.findElement(By.xpath("(//a[@aria-label='Carousel next slide'])[1]")).click();
+
+        ReuseableMethods.waiting(3);
+
         driver.findElement(By.xpath("(//div/ul)[1]/li[10]")).click();
+
+        ReuseableMethods.waiting(3);
 
 
     }
